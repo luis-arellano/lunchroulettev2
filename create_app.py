173464ef2d1947927b9b1c5b53e9bf1f9708ec1d
Luis Arellano <arellano.luis88@gmail.com>
2023-03-19 13:53:52 -0700

    clean create_app

--- create_app.py
+++ create_app.py
@@ -3,10 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-# db_uri = os.environ.get('DATABASE_URI')
-uri = 'mysql+mysqlconnector://root:rockclimber1!@database-1.ciiowaujelgi.us-west-2.rds.amazonaws.com:3306/lunchroulette'
+db_uri = os.environ.get('LUNCHROULETTE_URI')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 
 db = SQLAlchemy(app)
 
